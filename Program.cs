b8556598253e64116a28fa2bdff1733613b0c7a7
Nahid Bin Azhar <nahid.dns@gmail.com>
2025-07-30 14:31:46 +0600

    added: check latest version from github

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 using Apify.Commands;
 using Apify.Services;
+using NuGet.Versioning;
 using System.CommandLine;
+using System.Reflection;
+using System.Text.Json;
 
 namespace Apify
 {
@@ -8,6 +11,15 @@ namespace Apify
     {
         public async static Task<int> Main(string[] args)
         {
+            var version = Assembly.GetEntryAssembly()?
+                .GetCustomAttribute<AssemblyInformationalVersionAttribute>()?
+                .InformationalVersion ?? "1.0.0";
+            version = version.TrimStart('v');
+            var currentVersion = NuGetVersion.Parse(version);
+            await CheckForUpdate(currentVersion);
+            
+            
+            
             var rootCommand = new System.CommandLine.RootCommand
             {
                 Description = "Apify - A robust and powerful CLI tool for testing APIs and a mock server."
@@ -32,5 +44,41 @@ namespace Apify
             // Parse and execute
             return await rootCommand.InvokeAsync(args);
         }
+        
+        public async static Task<NuGetVersion?> GetLatestGitHubVersion()
+        {
+            using var client = new HttpClient();
+            client.DefaultRequestHeaders.UserAgent.ParseAdd("Apify CLI/1.0.0");
+
+            var url = $"https://api.github.com/repos/nahid/apify/releases/latest";
+            var json = await client.GetStringAsync(url);
+            using var doc = JsonDocument.Parse(json);
+            var tag = doc.RootElement.GetProperty("tag_name").GetString();
+            tag = tag?.TrimStart('v');
+
+            if (tag != null)
+                return NuGetVersion.Parse(tag);
+            
+            return null;
+        }
+        
+        
+        public async static Task CheckForUpdate(NuGetVersion currentVersion)
+        {
+            var latestVersion = await GetLatestGitHubVersion();
+
+            if (latestVersion != null && latestVersion > currentVersion)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("============================================================");
+                Console.WriteLine($"🚀 New version available: {latestVersion}");
+                Console.WriteLine("👉 Visit: https://github.com/nahid/apify/releases/latest");
+                Console.WriteLine("============================================================\n");
+                Console.ResetColor();
+            }
+        }
+
     }
+    
+
 }
